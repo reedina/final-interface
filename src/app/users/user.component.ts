b021d57bedfcf5b0eb9d0202900f4dd022f5e365
Reedina <reedinafarms@gmail.com>
2017-12-05 09:13:05 -0800

    updated part of the user form and it works

--- src/app/users/user.component.ts
+++ src/app/users/user.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem, DataTable, LazyLoadEvent } from "primeng/primeng";
+import { FormBuilder, FormGroup, Validators, AbstractControl } from "@angular/forms";
 
 import {Observable} from 'rxjs/Rx';
 import { UserService } from './user.service';
-import { IUser} from './user';
+import { IUser, User} from './user';
+import {ITeam, Team} from '../teams/team';
+import {Message} from 'primeng/primeng';
+import { TeamService } from '../teams/team.service';
+
 
 @Component({
   selector: 'app-user',
@@ -12,9 +17,23 @@ import { IUser} from './user';
 })
 export class UserComponent implements OnInit {
 
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  msgs: Message[] = [];
+  userForm: FormGroup;
+  user: User = new User()
   users: Array<IUser>;
+  teams: Array<ITeam>;  
+  resp: any;
 
-  constructor(private userService: UserService) { }
+  constructor(private fb: FormBuilder, private userService: UserService, private teamService: TeamService) { 
+    this.userForm = this.fb.group({
+      first_name:  ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      last_name:  ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],   
+      email:  ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern(this.emailPattern)]],           
+
+      });
+
+  }
 
   ngOnInit() {
 
@@ -22,5 +41,30 @@ export class UserComponent implements OnInit {
      do(res => console.log(res)).
      subscribe( res => { this.users = res });
 
+     this.teamService.getTeams().
+     do(res => console.log(res)).
+     subscribe( res => { this.teams = res;});
+
   }
+
+  showError(message: string) {
+    this.msgs = [];
+    this.msgs.push({severity: 'error', summary: 'Error Message', detail: message});
+}
+
+  showSuccess(name: string) {
+    this.msgs = [];
+    this.msgs.push({severity: 'success', summary: 'Success', detail: `Added ${this.truncate(name)}`});
+}
+
+  truncate(string){
+    if (string.length > 10)
+       return string.substring(0,10)+'...';
+    else
+       return string;
+}
+
+  onReset() {
+    this.userForm.reset();
+}
 }
--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -1,4 +1,13 @@
-import { ITeam } from  '../teams/team';
+import { ITeam, Team } from  '../teams/team';
+
+export class User implements IUser {
+    constructor(public id = 0, 
+        public first_name = '',
+        public last_name = '',
+        public email = '',
+        public team = new Team()) {}
+}
+
 
 export interface IUser {
     id: number;
