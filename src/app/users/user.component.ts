bd3a601a74ba1afedc12b2a0c325f114d14081ea
Reedina <reedinafarms@gmail.com>
2017-12-05 11:44:53 -0800

    user form working properly

--- src/app/users/user.component.ts
+++ src/app/users/user.component.ts
@@ -16,7 +16,7 @@ import { TeamService } from '../teams/team.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+teamsDd =  [];
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
   msgs: Message[] = [];
   userForm: FormGroup;
@@ -25,12 +25,14 @@ export class UserComponent implements OnInit {
   teams: Array<ITeam>;  
   resp: any;
 
+  teamsDropdown = [];
+
   constructor(private fb: FormBuilder, private userService: UserService, private teamService: TeamService) { 
     this.userForm = this.fb.group({
       first_name:  ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
       last_name:  ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],   
       email:  ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern(this.emailPattern)]],           
-
+      team: ['',[Validators.required]]
       });
 
   }
@@ -43,10 +45,22 @@ export class UserComponent implements OnInit {
 
      this.teamService.getTeams().
      do(res => console.log(res)).
-     subscribe( res => { this.teams = res;});
-
+     subscribe( res => { 
+       this.teams = res;
+       this.createTeamsDropdown();
+      });
+    
   }
 
+  createTeamsDropdown() {
+
+    this.teamsDd = [];
+    this.teamsDd.push({label: 'Select Team', value: null});
+     for ( let t  of this.teams)     {
+          this.teamsDd.push({label: t.name, value: t});
+    }
+
+  }
   showError(message: string) {
     this.msgs = [];
     this.msgs.push({severity: 'error', summary: 'Error Message', detail: message});
@@ -67,4 +81,33 @@ export class UserComponent implements OnInit {
   onReset() {
     this.userForm.reset();
 }
+
+updateUsers() {
+  this.userService.getUsers().
+  do(res => console.log(res)).
+  subscribe( res => { this.users = res });
+}
+
+save(): void {
+  if (this.userForm.dirty && this.userForm.valid) {
+    console.log('Attempting to Save: ' + JSON.stringify(this.userForm.value))
+    let t = Object.assign({}, this.user, this.userForm.value);
+    this.userService.saveUser(t)
+      .subscribe(
+         (r) => {console.log(`Successfully Saved Form: ${r.first_name}`);
+                 this.resp = r.first_name;
+                 this.updateUsers();
+                 this.userForm.reset();
+                 this.showSuccess(r.first_name);
+       },
+           (e) => {  this.userForm.reset(); this.showError(e);}
+
+      );
+  }  else {
+      // Remember, you only save a "valid" form
+      console.log("Form not dirty and valid")
+  }
+
+}
+
 }
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -22,4 +22,28 @@ export class UserService {
     .map(result => this.result  = result.json())
 }
 
+saveUser(user: IUser): Observable<IUser> {
+  let headers = new Headers({ 'Content-Type': 'application/json' });
+  let options = new RequestOptions({ headers: headers });
+
+  return this.createUser(user, options);
+}
+
+private createUser(user: IUser, options: RequestOptions): Observable<IUser> {
+  console.log(user)
+    return this.http.post("http://localhost:4040/api/user", user, options)
+    .map(this.extractData)
+    .do(data => console.log('create User: ' + JSON.stringify(data)))
+    .catch(this.handleError);
+  }
+
+  private extractData(response: Response) {
+    let body = response.json();
+    return body || {};
+  }
+  
+  private handleError(error: Response): Observable<any> {
+    console.error(error);  // Send to logging server at some point
+    return Observable.throw(error.json().error || 'Server error');
+  }
 }
