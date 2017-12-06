bfba4394701fde3caab814b5e67524c696a68a89
Reedina <reedinafarms@gmail.com>
2017-12-06 10:09:06 -0800

    added form for build instance - initial part of form

--- src/app/environment-instances/environment-instances.component.ts
+++ src/app/environment-instances/environment-instances.component.ts
@@ -18,20 +18,98 @@ import { EnvironmentService } from '../environments/environment.service';
 })
 export class EnvironmentInstancesComponent implements OnInit {
 
+// var newDate = addDays(new Date(), 5);
+  minDeleteDate =  this.addDays(new Date(), 1);
+  environmentsDd = [];
   msgs: Message[] = [];
-  
+  environmentInstanceForm: FormGroup;
   environmentInstance: EnvironmentInstance = new EnvironmentInstance()
   environmentInstances: Array<IEnvironment>;
   environments: Array<IEnvironment>;  
   resp: any;
 
 
-  constructor(private environmentInstanceService: EnvironmentInstancesService, private environmentService: EnvironmentService) { }
+  constructor(private fb: FormBuilder,private environmentInstanceService: EnvironmentInstancesService, private environmentService: EnvironmentService) {
+    this.environmentInstanceForm = this.fb.group({
+      name:  ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      deletion_time:  ['', [Validators.required]],
+      environment: ['', [Validators.required]]
+      });
+   }
 
   ngOnInit() {
     this.environmentInstanceService.getEnvironmentInstances().
     do(res => console.log(res)).
     subscribe( res => { this.environmentInstances = res });
+
+    this.environmentService.getEnvironments().
+    do(res => console.log(res)).
+    subscribe( res => { 
+      this.environments = res;
+      this.createEnvironmentsDropdown();
+     });
   }
 
+  createEnvironmentsDropdown() {
+    
+        this.environmentsDd = [];
+        this.environmentsDd.push({label: 'Select Environment', value: null});
+         for ( let t  of this.environments)     {
+               let labelName = t.name + " - (" + t.type + ")";
+              this.environmentsDd.push({label: labelName, value: t});
+        }
+    
+      }
+
+  showError(message: string) {
+    this.msgs = [];
+    this.msgs.push({severity: 'error', summary: 'Error Message', detail: message});
+}
+
+  showSuccess(name: string) {
+    this.msgs = [];
+    this.msgs.push({severity: 'success', summary: 'Success', detail: `Added ${this.truncate(name)}`});
+}
+truncate(string){
+  if (string.length > 10)
+     return string.substring(0,10)+'...';
+  else
+     return string;
+}
+
+onReset() {
+  this.environmentInstanceForm.reset();
+}
+
+updateEnvironmentInstances() {
+  this.environmentInstanceService.getEnvironmentInstances().
+  do(res => console.log(res)).
+  subscribe( res => { this.environmentInstances = res });
+}
+
+save(): void {
+  if (this.environmentInstanceForm.dirty && this.environmentInstanceForm.valid) {
+    console.log('Attempting to Save: ' + JSON.stringify(this.environmentInstanceForm.value))
+    let t = Object.assign({}, this.environmentInstance, this.environmentInstanceForm.value);
+    this.environmentInstanceService.saveEnvironmentInstance(t)
+      .subscribe(
+         (r) => {console.log(`Successfully Saved Form: ${r.name}`);
+                 this.resp = r.name;
+                 this.updateEnvironmentInstances();
+                 this.environmentInstanceForm.reset();
+                 this.showSuccess(r.name);
+       },
+           (e) => {  this.environmentInstanceForm.reset(); this.showError(e);}
+
+      );
+  }  else {
+      // Remember, you only save a "valid" form
+      console.log("Form not dirty and valid")
+  }
+
+}
+
+addDays(theDate, days) {
+  return new Date(theDate.getTime() + days*24*60*60*1000);
+}
 }
--- src/app/environment-instances/environment-instances.service.ts
+++ src/app/environment-instances/environment-instances.service.ts
@@ -21,4 +21,29 @@ export class EnvironmentInstancesService {
     return this.http.get('http://localhost:4050/api/environment/instances')
     .map(result => this.result  = result.json())
 }
+
+saveEnvironmentInstance(user: IEnvironmentInstance): Observable<IEnvironmentInstance> {
+  let headers = new Headers({ 'Content-Type': 'application/json' });
+  let options = new RequestOptions({ headers: headers });
+
+  return this.createEnvironmentInstance(user, options);
+}
+
+private createEnvironmentInstance(user: IEnvironmentInstance, options: RequestOptions): Observable<IEnvironmentInstance> {
+  console.log(user)
+    return this.http.post("http://localhost:4050/api/environment/instance", user, options)
+    .map(this.extractData)
+    .do(data => console.log('create Environment Instance: ' + JSON.stringify(data)))
+    .catch(this.handleError);
+  }
+
+  private extractData(response: Response) {
+    let body = response.json();
+    return body || {};
+  }
+  
+  private handleError(error: Response): Observable<any> {
+    console.error(error);  // Send to logging server at some point
+    return Observable.throw(error.json().error || 'Server error');
+  }
 }
